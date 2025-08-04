1777bc6a2fb2cfd3a608cb6dc746d52e3e96a53b
KG-0920 <t6946158@gmail.com>
2025-08-04 19:40:01 +0900

    Update Program.cs

--- APP_A/Program.cs
+++ APP_A/Program.cs
@@ -1,41 +1,17 @@
 using System;
-using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace APP_A
 {
-    internal class Program
+    internal static class Program
     {
-        [DllImport("IoboardEmulator.dll", CallingConvention = CallingConvention.StdCall)]
-        public static extern int RegisterDioHandle([MarshalAs(UnmanagedType.LPStr)] string boardName);
-
-        [DllImport("IoboardEmulator.dll", CallingConvention = CallingConvention.StdCall)]
-        public static extern void UnregisterDioHandle();
-
-        [DllImport("IoboardEmulator.dll", CallingConvention = CallingConvention.StdCall)]
-        public static extern void SetOutput(int port, int value);
-
-        [DllImport("IoboardEmulator.dll", CallingConvention = CallingConvention.StdCall)]
-        public static extern int GetInput(int port);
-
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("APP_A started.");
-
-            Console.WriteLine("Registering handle...");
-            int result = RegisterDioHandle("FBIDIO0");
-            Console.WriteLine($"RegisterDioHandle result = {result}");
-
-            Console.WriteLine("Setting output port 1 to 1");
-            SetOutput(1, 1);
-
-            Console.WriteLine("Getting input port 1");
-            int input = GetInput(1);
-            Console.WriteLine($"GetInput result = {input}");
-
-            Console.WriteLine("Unregistering handle...");
-            UnregisterDioHandle();
-
-            Console.WriteLine("APP_A finished.");
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
         }
     }
 }
