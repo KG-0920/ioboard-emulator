9f6946b1b7de44ac4ec85d3f38ebca5a2e7318db
KG-0920 <t6946158@gmail.com>
2025-08-05 17:57:32 +0900

    Create IoboardSetting.cs

---
+++ SharedConfig/IoboardSetting.cs
@@ -0,0 +1,9 @@
+namespace SharedConfig
+{
+    public class IoboardSetting
+    {
+        public int RotarySwitchNo { get; set; }
+        public int InputPortCount { get; set; }
+        public int OutputPortCount { get; set; }
+    }
+}
